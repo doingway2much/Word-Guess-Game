6082a4fc89863adc36be3043e47dde3b6a9fcdef
Seth Randell <sethhrandell@gmail.com>
2019-06-17 14:09:22 -0400

    added bootstrap buttons to the html page and code topsuh data to them

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -4,150 +4,159 @@
 
 var songs = [
     {
-    title: "still not a player",
-    artist: "Big Punisher ft Joe",
-    wikiLink: "https://en.wikipedia.org/wiki/Still_Not_a_Player",
-    fact1: "This song was released in 1998 by this Puerto Rican native",
-    fact2: "The rapper passed away in 2000",
-    fact3: "The rappers was on the Terror Squad label"
-},
-{
-    title: "gin and juice",
-    artist: "Snoop Dogg",
-    wikiLink: "https://en.wikipedia.org/wiki/Gin_and_Juice",
-    fact1: "Song was released in 1994 but this Long Beach native",
-    fact2: "This song features a sample from George McCrae's I Get Lifted",
-    fact3: "The star of this song now has a TV show staring Martha Stuwart"
-},
-{
-    title: "it was a good day",
-    artist: "Ice Cube",
-    wikiLink: "https://en.wikipedia.org/wiki/It_Was_a_Good_Day",
-    fact1: "Song was released in 1993 by this Los Angels native",
-    fact2: "This song features a gangster rapper turned movie star",
-    fact3: "This music video feature a famous model Chevy Impala"
-},
-{
-    title: "ill be there for you",
-    artist: "Method Man ft. Mary J. Blige",
-    wikiLink: "https://en.wikipedia.org/wiki/I%27ll_Be_There_for_You/You%27re_All_I_Need_to_Get_By",
-    fact1: "Song was release in 1995 by this unique pair from New York",
-    fact2: "One of the artists is a member of the Wu-Tang-Clan",
-    fact3: "The other R&B artist was divorced her long time manager"
-},
-   { 
-    title: "regulate",
-    artist: "Warren G ft Nate Dogg",
-    wikiLink: "https://en.wikipedia.org/wiki/Regulate_(song)",
-    fact1: "This song was released in 1993 by another not as famous Long Beach native",
-    fact2: "This song was featured on the Above the Rim Soundtrack",
-    fact3: "This song also features a Compton rapper passed away in 2011"
+        title: "still not a player",
+        artist: "Big Punisher ft Joe",
+        wikiLink: "https://en.wikipedia.org/wiki/Still_Not_a_Player",
+        fact1: "This song was released in 1998 by this Puerto Rican native",
+        fact2: "The rapper passed away in 2000",
+        fact3: "The rappers was on the Terror Squad label"
+    },
+    {
+        title: "gin and juice",
+        artist: "Snoop Dogg",
+        wikiLink: "https://en.wikipedia.org/wiki/Gin_and_Juice",
+        fact1: "Song was released in 1994 but this Long Beach native",
+        fact2: "This song features a sample from George McCrae's I Get Lifted",
+        fact3: "The star of this song now has a TV show staring Martha Stuwart"
+    },
+    {
+        title: "it was a good day",
+        artist: "Ice Cube",
+        wikiLink: "https://en.wikipedia.org/wiki/It_Was_a_Good_Day",
+        fact1: "Song was released in 1993 by this Los Angels native",
+        fact2: "This song features a gangster rapper turned movie star",
+        fact3: "This music video feature a famous model Chevy Impala"
+    },
+    {
+        title: "ill be there for you",
+        artist: "Method Man ft. Mary J. Blige",
+        wikiLink: "https://en.wikipedia.org/wiki/I%27ll_Be_There_for_You/You%27re_All_I_Need_to_Get_By",
+        fact1: "Song was release in 1995 by this unique pair from New York",
+        fact2: "One of the artists is a member of the Wu-Tang-Clan",
+        fact3: "The other R&B artist was divorced her long time manager"
+    },
+    {
+        title: "regulate",
+        artist: "Warren G ft Nate Dogg",
+        wikiLink: "https://en.wikipedia.org/wiki/Regulate_(song)",
+        fact1: "This song was released in 1993 by another not as famous Long Beach native",
+        fact2: "This song was featured on the Above the Rim Soundtrack",
+        fact3: "This song also features a Compton rapper passed away in 2011"
     }
 ];
 
-    // Game variables
-    var numberOfWins = 0;
-    var losses = 0;
-    var guesses = 0;
-    var songsList = [];
-    var userGuessed = [];
-    var randomSong = "";
-    var randomSongEmpty = [];
-    var lettersInSong = [];
-    var numOfBlanks = [];
-    var alphabet = "abcdefghijklmnopqrstuvwxyz";
-    var underScore = [];
-    var numSpaces = 0;
-    var progress = [];
-    var numGuess = 20;
-    // var getRandomSong = "";
+// Game variables
+var numberOfWins = 0;
+var losses = 0;
+var guesses = 0;
+var songsList = [];
+var userGuessed = [];
+var randomSong = "";
+var randomSongEmpty = [];
+var lettersInSong = [];
+var numOfBlanks = [];
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var underScore = [];
+var numSpaces = 0;
+var progress = [];
+var numGuess = 20;
+// var getRandomSong = "";
 
-    // Gets lists of song titles from the object
-    var getList = function() {
-        for (var i = 0; i < songs.length; i++) {
-            songsList.push(songs[i].title + "");
-        }
+// Gets lists of song titles from the object
+var getList = function () {
+    for (var i = 0; i < songs.length; i++) {
+        songsList.push(songs[i].title + "");
     }
-    getList();
-    
-    // Gets random song  from that list
-    randomSong = songsList[Math.floor(Math.random() * songsList.length)];
-    numOfBlanks = lettersInSong.length;
-    console.log(randomSong);
+}
+getList();
 
-    // Puts all the letters from the song in with a new aray letter by letter and spirts it out as new var with hidden checters
-    lettersInSong = randomSong.split("");
+// Sets origonal value to wrong button
+var looserButtonStart = document.getElementById("wrong");
+        looserButtonStart.textContent = numberOfWins;
 
-    for( var i = 0; i < lettersInSong.length; i++)
-        if(lettersInSong[i] === " ") {
-            underScore.push("*");
-        } else {
-            underScore.push ("_");
-            
-        }
-    console.log(underScore);
-    console.log(lettersInSong);
+// Sets origonal value to right button
+var winnerButtonStart = document.getElementById("right");
+        winnerButtonStart.textContent = numberOfWins;
 
-    //  If somg title is a match dispaly data for the random song
-        if(randomSong === songs[0].title) {
-            console.log(songs[0].fact1);
-            var song1Selected = document.getElementById("game");            
-            song1Selected.textContent = "Here's yoru first hint: " + songs[0].fact1;
-            var song1Title = document.getElementById("selected");
-            song1Title.textContent = underScore;
-           }
-        if(randomSong === songs[1].title) {
-            console.log(songs[1].fact1);
-            var song1Selected = document.getElementById("game");            
-            song1Selected.textContent = "Here's yoru first hint: " + songs[1].fact1;
-            var song1Title = document.getElementById("selected");
-            song1Title.textContent = underScore;
-            if(numGuess === 15) {
-                var song1Selected = document.getElementById("game");            
-                song1Selected.textContent = "Here's yoru second hint: " + songs[1].fact2;
-        }}
-        if(randomSong === songs[2].title) {
-            console.log(songs[2].fact1);
-            var song1Selected = document.getElementById("game");            
-            song1Selected.textContent = "Here's yoru first hint: " + songs[2].fact1;
-            var song1Title = document.getElementById("selected");
-            song1Title.textContent = underScore;
-        }
-        if(randomSong === songs[3].title) {
-            console.log(songs[3].fact1);
-            var song1Selected = document.getElementById("game");            
-            song1Selected.textContent = "Here's yoru first hint: " + songs[3].fact1;
-            var song1Title = document.getElementById("selected");
-            song1Title.textContent = underScore;
-        }
-        if(randomSong === songs[4].title) {
-            console.log(songs[4].fact1);
-            var song1Selected = document.getElementById("game");            
-            song1Selected.textContent = "Here's yoru first hint: " + songs[4].fact1;
-            var song1Title = document.getElementById("selected");
-            song1Title.textContent = underScore;
-        }
-        
-        var newGame = document.getElementById("instructions");
-        newGame.textContent = "This is a new game";
+// Gets random song  from that list
+randomSong = songsList[Math.floor(Math.random() * songsList.length)];
+numOfBlanks = lettersInSong.length;
+console.log(randomSong);
 
-        // Create user variable for user guess on key press
-        document.onkeyup = function(event) {
-        var userGuess = event.key
-        userGuessed.push(userGuess);
+// Puts all the letters from the song in with a new aray letter by letter and spirts it out as new var with hidden checters
+lettersInSong = randomSong.split("");
 
-        
-        // Add guess to history
-        var guess = document.getElementById("history");
-        guess.textContent = userGuessed;
+for (var i = 0; i < lettersInSong.length; i++)
+    if (lettersInSong[i] === " ") {
+        underScore.push(" ");
+    } else {
+        underScore.push("_");
 
-        var letterInWord = false;
-        console.log(letterInWord);
+    }
+console.log(underScore);
+console.log(lettersInSong);
 
-        console.log("The user guessed " + userGuess);      
+//  If somg title is a match dispaly data for the random song
+if (randomSong === songs[0].title) {
+    console.log(songs[0].fact1);
+    var song1Selected = document.getElementById("game");
+    song1Selected.textContent = "Here's your first hint: " + songs[0].fact1;
+    var song1Title = document.getElementById("selected");
+    song1Title.textContent = underScore;
+}
+if (randomSong === songs[1].title) {
+    console.log(songs[1].fact1);
+    var song1Selected = document.getElementById("game");
+    song1Selected.textContent = "Here's your first hint: " + songs[1].fact1;
+    var song1Title = document.getElementById("selected");
+    song1Title.textContent = underScore;
+    if (numGuess === 15) {
+        var song1Selected = document.getElementById("game");
+        song1Selected.textContent = "Here's your second hint: " + songs[1].fact2;
+    }
+}
+if (randomSong === songs[2].title) {
+    console.log(songs[2].fact1);
+    var song1Selected = document.getElementById("game");
+    song1Selected.textContent = "Here's your first hint: " + songs[2].fact1;
+    var song1Title = document.getElementById("selected");
+    song1Title.textContent = underScore;
+}
+if (randomSong === songs[3].title) {
+    console.log(songs[3].fact1);
+    var song1Selected = document.getElementById("game");
+    song1Selected.textContent = "Here's your first hint: " + songs[3].fact1;
+    var song1Title = document.getElementById("selected");
+    song1Title.textContent = underScore;
+}
+if (randomSong === songs[4].title) {
+    console.log(songs[4].fact1);
+    var song1Selected = document.getElementById("game");
+    song1Selected.textContent = "Here's your first hint: " + songs[4].fact1;
+    var song1Title = document.getElementById("selected");
+    song1Title.textContent = underScore;
+}
 
-        for( var i = 0; i < lettersInSong.length; i++) {
-        if(lettersInSong[i] === userGuess) {
+var newGame = document.getElementById("instructions");
+newGame.textContent = "This is a new game";
+
+// Create user variable for user guess on key press
+document.onkeyup = function (event) {
+    var userGuess = event.key
+    userGuessed.push(userGuess);
+
+
+    // Add guess to history
+    var guess = document.getElementById("history");
+    guess.textContent = userGuessed;
+
+    var letterInWord = false;
+    console.log(letterInWord);
+
+    console.log("The user guessed " + userGuess);
+
+    for (var i = 0; i < lettersInSong.length; i++) {
+        if (lettersInSong[i] === userGuess) {
             letterInWord = true;
             console.log(letterInWord);
             underScore[i] = userGuess;
@@ -157,25 +166,71 @@ var songs = [
         else {
             letterInWord = false;
             console.log(letterInWord);
-        }}
-        if(!letterInWord){
-            numGuess--;
-            console.log(numGuess);
-            var remaining1 = document.getElementById("remaining");
-            remaining1.textContent = numGuess;
         }
-        if(numGuess === 15) {
-            var song1Selected = document.getElementById("game");            
-            song1Selected.textContent = "Here's yoru second hint: " + songs[0].fact2;
     }
 
-        
-    };
-        // console.log(underScore);
-        
-            // for(var i = 0; i < numOfBlanks; i++);
-            //     if (lettersInSong[i] === userGuess) {
-            //     var containsLetter = true;
-            //     console.log(containsLetter);
-            // }
-        
+
+    if (!letterInWord) {
+        numGuess--;
+        console.log(numGuess);
+        var remaining1 = document.getElementById("remaining");
+        remaining1.textContent = numGuess;
+    }
+
+    //  Adds hints to the page when they hit 15 and 10 guesses 
+    if ((numGuess === 15) && (randomSong === songs[0].title)) {
+        var song1Selectedf2 = document.getElementById("fact2");
+        song1Selectedf2.textContent = "Here's yoru second hint: " + songs[0].fact2;
+    }
+    if ((numGuess === 10) && (randomSong === songs[0].title)) {
+        var song1Selectedf2 = document.getElementById("fact3");
+        song1Selectedf2.textContent = "Here's yoru third hint: " + songs[0].fact3;
+    }
+    if ((numGuess === 15) && (randomSong === songs[1].title)) {
+        var song1Selectedf2 = document.getElementById("fact2");
+        song1Selectedf2.textContent = "Here's yoru second hint: " + songs[1].fact2;
+    }
+    if ((numGuess === 10) && (randomSong === songs[1].title)) {
+        var song1Selectedf2 = document.getElementById("fact3");
+        song1Selectedf2.textContent = "Here's yoru third hint: " + songs[1].fact3;
+    }
+    if ((numGuess === 15) && (randomSong === songs[2].title)) {
+        var song1Selectedf2 = document.getElementById("fact2");
+        song1Selectedf2.textContent = "Here's yoru second hint: " + songs[2].fact2;
+    }
+    if ((numGuess === 10) && (randomSong === songs[2].title)) {
+        var song1Selectedf2 = document.getElementById("fact3");
+        song1Selectedf2.textContent = "Here's yoru third hint: " + songs[2].fact3;
+    }
+    if ((numGuess === 15) && (randomSong === songs[3].title)) {
+        var song1Selectedf2 = document.getElementById("fact2");
+        song1Selectedf2.textContent = "Here's yoru second hint: " + songs[3].fact2;
+    }
+    if ((numGuess === 10) && (randomSong === songs[3].title)) {
+        var song1Selectedf2 = document.getElementById("fact3");
+        song1Selectedf2.textContent = "Here's yoru third hint: " + songs[3].fact3;
+    }
+    if ((numGuess === 15) && (randomSong === songs[4].title)) {
+        var song1Selectedf2 = document.getElementById("fact2");
+        song1Selectedf2.textContent = "Here's yoru second hint: " + songs[4].fact2;
+    }
+    if ((numGuess === 10) && (randomSong === songs[4].title)) {
+        var song1Selectedf2 = document.getElementById("fact3");
+        song1Selectedf2.textContent = "Here's yoru third hint: " + songs[4].fact3;
+    }
+    if(numGuess < 1) {
+        losses++;
+        var looser = document.getElementById("loose");
+        looser.textContent = losses;
+        var looserButton = document.getElementById("wrong");
+        looserButton.textContent = losses;
+    }
+    if (lettersInSong.toString() === underScore.toString()) {
+        console.log("You WIN!!!!!!");
+        numberOfWins++;
+        var winner = document.getElementById("wins");
+        winner.textContent = numberOfWins;
+}
+    console.log(underScore);
+   
+};
